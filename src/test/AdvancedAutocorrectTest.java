13e5d694437c6eae4acdc9c6296964a9440e6eb5
Daniel Park <dan@vpn-128-97-245-30.host.ucla.edu>
2018-11-19 14:29:56 -0800

    JUnit test for AdvancedAutocorrect

---
+++ src/test/AdvancedAutocorrectTest.java
@@ -0,0 +1,21 @@
+package test;
+import main.*;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import java.util.List;
+
+public class AdvancedAutocorrectTest {
+	Data data = Data.getInstance();
+	String[] correct = {"on","any","hello", "world", "oracle", "testing", "example"};
+	boolean verbose = false; // Set true to print all test cases
+	
+	
+	@Test
+	public void combineAgain() {
+		
+		
+	}
+	
+	
+}
+	
